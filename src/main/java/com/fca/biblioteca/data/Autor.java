bc5fce23ba9e5e630b542e9038b6129a27581321
DevResident <fernando.barcenax@gmail.com>
2025-09-01 19:17:48 -0600

    import jakarta.persistence.*; -> import jakarta.persistence.(cual se usa); Para no traer toda la biblioteca

--- src/main/java/com/fca/biblioteca/data/Autor.java
+++ src/main/java/com/fca/biblioteca/data/Autor.java
@@ -1,4 +1,11 @@
-import jakarta.persistence.*;
+package com.fca.biblioteca.data;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
 
 @Entity
 @Table(name = "autor")
