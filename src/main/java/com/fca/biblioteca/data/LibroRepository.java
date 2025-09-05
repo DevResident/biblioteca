6bb7aabcd534ae2432865ca8c97d0101a4d405fc
DevResident <fernando.barcenax@gmail.com>
2025-09-05 10:44:39 -0600

    buscarPorTitulo con el metodo .stream

--- src/main/java/com/fca/biblioteca/data/LibroRepository.java
+++ src/main/java/com/fca/biblioteca/data/LibroRepository.java
@@ -3,6 +3,8 @@ package com.fca.biblioteca.data;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LibroRepository extends JpaRepository<Libro, Integer> {
 }
--- src/main/java/com/fca/biblioteca/domain/LibroDomain.java
+++ src/main/java/com/fca/biblioteca/domain/LibroDomain.java
@@ -6,14 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class LibroDomain {
 
     @Autowired
     private LibroRepository libroRepository;
-    private List<Libro> buscarLibroPorTitulo(String titulo) {
-        return libroRepository.findAll();
+
+    public List<Libro> buscarLibroPorTitulo(String titulo, String edicion) {
+        return libroRepository.findAll()
+                .stream()
+                .filter(Libro -> Libro.getTitulo().equals(titulo))
+                .filter(Libro -> Libro.getEdicion().equals(edicion))
+                .collect(Collectors.toList());
     }
 
 }
